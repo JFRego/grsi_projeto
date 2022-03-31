88fe24cd220d5c0d23bbc020c2a449110fa90aa7
JFRego <joserego@enta.pt>
2022-03-31 16:23:12 +0000

    app feita

---
+++ xedni.js
@@ -0,0 +1,23 @@
+//basic http server
+
+const { response } = require('express');
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static('./public)'));
+
+//rota principal
+app.get('/', (request, response) => {
+    response.sendFile(path.join(__dirname,('./public/index.html')))
+})
+
+const port = 3000;
+app.listen(port);
+
+
+
+
+
+
+
